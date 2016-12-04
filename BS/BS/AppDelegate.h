ace8efe414ea21d3b831e6143c0fe02c9f39c94f
俊哥哥 <jungege@jungegedeMacBook-Air.local>
2016-12-04 11:13:58 +0800

    连接

---
+++ BS/BS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BS
+//
+//  Created by 俊哥哥 on 2016/12/4.
+//  Copyright © 2016年 俊哥哥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BS/BS/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  BS
+//
+//  Created by 俊哥哥 on 2016/12/4.
+//  Copyright © 2016年 俊哥哥. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"连接");
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
